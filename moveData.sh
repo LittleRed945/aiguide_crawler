23de55d628b1ac7c2bebd438aeb4416fa67a4b6a
train_237 <t110598082@ntut.org.tw>
2025-07-11 09:29:16 +0800

    add reset and move data to directory shell script

---
+++ moveData.sh
@@ -0,0 +1,8 @@
+TARGET_DIR=/home/selab/Documents/ShuTing/Research/rltmp
+
+mkdir -p $TARGET_DIR/log
+mv log/* $TARGET_DIR/log
+mkdir -p $TARGET_DIR/directiveTreeGraphic
+mv directiveTreeGraphic/* $TARGET_DIR/directiveTreeGraphic
+mkdir -p $TARGET_DIR/crawler-record
+mv crawler-record/* $TARGET_DIR/crawler-record
---
+++ reset.sh
@@ -0,0 +1,3 @@
+rm -rf log/* ^/dev/null
+rm -rf directiveTreeGraphic/* ^/dev/null
+rm -rf crawler-record/* ^/dev/null
